65f5c0b55b71ace61fa063a37f2b92c71011652d
vnsvakanda <phinyaphak.c@ku.th>
2021-10-17 20:50:00 +0700

    add method in rental and write more unit test

--- rental.py
+++ rental.py
@@ -1,24 +1,33 @@
+from movie import *
+
+
 class Rental:
-	"""
-	A rental of a movie by customer.
-	From Fowler's refactoring example.
+    """
+    A rental of a movie by customer.
+    From Fowler's refactoring example.
 
-	A realistic Rental would have fields for the dates
-	that the movie was rented and returned, from which the
-	rental period is calculated.
-	But for simplicity of the example only a days_rented
-	field is used.
-	"""
-	
-	def __init__(self, movie, days_rented): 
-		"""Initialize a new movie rental object for
-		   a movie with known rental period (daysRented).
-		"""
-		self.movie = movie
-		self.days_rented = days_rented
+    A realistic Rental would have fields for the dates
+    that the movie was rented and returned, from which the
+    rental period is calculated.
+    But for simplicity of the example only a days_rented
+    field is used.
+    """
 
-	def get_movie(self):
-		return self.movie
+    def __init__(self, movie, days_rented):
+        """Initialize a new movie rental object for
+           a movie with known rental period (daysRented).
+        """
+        self.movie = movie
+        self.days_rented = days_rented
 
-	def get_days_rented(self):
-		return self.days_rented
+    def get_movie(self):
+        return self.movie
+
+    def get_days_rented(self):
+        return self.days_rented
+
+    def get_price(self):
+        return self.get_movie().get_price_code().price(self.get_days_rented())
+
+    def get_point(self):
+        return self.get_movie().get_price_code().points(self.get_days_rented())
